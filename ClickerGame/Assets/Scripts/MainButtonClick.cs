21d86599041b89ec38deb1a99f4fd0b255fdcc53
TheSchlote <TheSchlote@gmail.com>
2020-05-25 12:55:20 -0500

    Inital Upload
    Inital upload of a Cookie Clicker, Later to be converted into other things like NickClicker

---
+++ ClickerGame/Assets/Scripts/MainButtonClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainButtonClick : MonoBehaviour
+{
+    public GameObject textBox;
+
+
+    public void ClickTheButton()
+    {
+        GlobalCookies.CookieCount += 1;
+    }
+}
