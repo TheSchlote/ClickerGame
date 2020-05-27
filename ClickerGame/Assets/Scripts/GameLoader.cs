5c0f132b90f5e7a2e200328e74c575f0cf7d8693
TheSchlote <TheSchlote@gmail.com>
2020-05-27 00:00:52 -0500

    Finished Tutorial
    Now to make it NickClicker in its final form!

---
+++ ClickerGame/Assets/Scripts/GameLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoader : MonoBehaviour
+{
+    public int savedCookies;
+    public int savedCash;
+    public int savedBakers;
+    public int savedBakersPerSec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(MainMenu.isLoading == true)
+        {
+            savedCookies = PlayerPrefs.GetInt("SavedCookies");
+            GlobalCookies.CookieCount = savedCookies;
+
+            savedCash = PlayerPrefs.GetInt("SavedCash");
+            GlobalCash.CashCount = savedCash;
+
+            savedBakers = PlayerPrefs.GetInt("NumberOfBakers");
+            GlobalBaker.numberOfBakers = savedBakers;
+
+            savedBakersPerSec = PlayerPrefs.GetInt("BakersPerSec");
+            GlobalBaker.bakersPerSec = savedBakersPerSec;
+        }
+    }
+}
---
+++ ClickerGame/Assets/Scripts/SaveGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveGame : MonoBehaviour
+{
+    //public int saveGameCash;
+    public Button saveButton;
+    public Text saveText;
+
+    void Update()
+    {
+        
+    }
+
+    public void SaveTheGame()
+    {
+        PlayerPrefs.SetInt("SavedCookies", GlobalCookies.CookieCount);
+        PlayerPrefs.SetInt("SavedCash", GlobalCash.CashCount);
+        PlayerPrefs.SetInt("NumberOfBakers", GlobalBaker.numberOfBakers);
+        PlayerPrefs.SetInt("BakersPerSec", GlobalBaker.bakersPerSec);
+    }
+}
